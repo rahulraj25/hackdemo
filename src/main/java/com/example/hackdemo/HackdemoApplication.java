a16248b847d6ec5df2c01f88e0919bf0e483abc1
Rahul Raj <rahulrajvansh9708@gmail.com>
2021-02-25 20:14:44 +0530

    Add files via upload

---
+++ src/main/java/com/example/hackdemo/HackdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hackdemo/HackdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
